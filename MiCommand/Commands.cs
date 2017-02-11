0f916bdf55f648db6ce7db41d9f9b19a3d3cfafc
Sepi <deet1022@gmail.com>
2017-02-11 17:53:16 +0900

    Help 명령어 추가

---
+++ MiCommand/Commands.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiNET;
+using MiNET.Plugins;
+using MiNET.Plugins.Attributes;
+using MiNET.Net;
+using MiNET.Utils;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using log4net;
+using System.Reflection;
+
+namespace MiCommand
+{
+    public class Commands : MiCommand
+    {
+        [Command]
+        public void Help(Player player)
+        {
+
+        }
+    }
+}
--- MiCommand/MiCommand.cs
+++ MiCommand/MiCommand.cs
@@ -31,24 +31,5 @@ namespace MiCommand
         {
             Log.Info("MiCommand가 언로드되었습니다.");
         }
-
-        [Command]
-        public void Test(Player player)
-        {
-            player.SendMessage("성공");
-        }
-
-        [Command]
-        public void Cmd(Player player)
-        {
-            player.SendMessage("cmd 명령어임.");
-        }
-
-        [Command]
-        public void Test(Player player, string args1, string args2)
-        {
-            player.SendMessage("성공");
-            player.SendMessage($"인자: {args1}, {args2}");
-        }
     }
 }
